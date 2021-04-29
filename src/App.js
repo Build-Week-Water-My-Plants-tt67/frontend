3f58a1756aa26159a70f2f9d3e21624e5a14d456
Your Name <you@example.com>
2021-04-29 02:00:51 +0100

    Header Conflict Resolved

--- src/App.js
+++ src/App.js
@@ -1,72 +1,22 @@
 import React, {useState } from "react";
-import SignUp from './components/SignUp';
-import Login from './components/Login';
 import { Link } from 'react-router-dom';
 import EditUser from './components/EditUser';
 import CreatePlant from "./components/CreatePlant";
 import Home from "./components/Home";
-import styled from 'styled-components';
+
+import Header from "./components/Header";
 
 const App = () => {
  
-  const StyledHeader = styled.div`
-  width: 100%;
-  display: flex;
-  justify-content: space-between;
-  background-color: #1f441e;
 
-  nav{
-    width: 40%;
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    
-  a{
-    color: white;
-    text-decoration: none;
-    font-weight: bold;
-  }
-  }
-`
-const StyledLogo = styled.div`
-  
-  img{
-    width: 20%;
-  }
-`
-const StyledLink = styled.div`
-
-      display: flex;
-      justify-content: left;
-      width: 30%;
-      border-radius: 2%;
-      margin-bottom: 0.5%;
-      padding: 0.2%;
-
-    label{
-      color: white;
-    }
 
     
-`
+
 
   return (
     <div className="App">
-   <StyledHeader>
-       <StyledLogo>
-       <img src='./components/images/kelly-sikkema-SaJzwm0xR9c-unsplash.jpg'/>
-       </StyledLogo>
-       <nav>
-         <Link to={'/'}>Home</Link>
-         <Link>Gallery</Link>
-         <Link>Contact</Link>
-       </nav>
-       <StyledLink>
-        <Link to={`/login`}>
-          <Login/>
-        </Link>
-        </StyledLink>
-      </StyledHeader>
+      <Header/>
+   
 
       {/* {(user.phoneNumber != "") ? (
         <div className= "welcome">
